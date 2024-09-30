bf0c675d19dc343533d7aadeaa73d910561eeee3
Minijackson <minijackson@riseup.net>
2024-09-30 14:56:39 +0200

    phoebus-ecosystem-image: limit mem usage of tomcat, kafka, elasticsearch

--- pkgs/phoebus-ecosystem-image/configuration/phoebus-alarm.nix
+++ pkgs/phoebus-ecosystem-image/configuration/phoebus-alarm.nix
@@ -35,6 +35,7 @@ in
       # nix shell 'nixpkgs#apacheKafka' -c kafka-storage.sh random-uuid
       clusterId = "8MEBIhkRS963am5l4DsQlQ";
       formatLogDirs = true;
+      jvmOptions = ["-Xmx256m"];
       settings = {
         listeners = [
           # This is the address Kafka will listen to,
