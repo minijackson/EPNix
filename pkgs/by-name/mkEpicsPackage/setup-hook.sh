a2e34371c0b985fe28ef6befeb59d29fdedd60c7
Minijackson <minijackson@riseup.net>
2026-01-16 13:56:26 +0100

    mkEpicsPackage: use extendMkDerivation
    This is the standard function to extend the `stdenv.mkDerivation`
    function, and allows the use of the `mkEpicsPackage (finalAttrs: ...)`
    pattern.

--- pkgs/by-name/mkEpicsPackage/setup-hook.sh
+++ pkgs/by-name/mkEpicsPackage/setup-hook.sh
@@ -2,4 +2,4 @@
 # so that we are able to list every EPICS-specific packages,
 # to add them in the 'configure/RELEASE.local' file
 # in dependent packages.
-export "EPICS_COMPONENTS=${EPICS_COMPONENTS:+${EPICS_COMPONENTS}:}@varname@=@out@"
+export "EPICS_COMPONENTS=${EPICS_COMPONENTS:+${EPICS_COMPONENTS}:}@component_varname@=@out@"
