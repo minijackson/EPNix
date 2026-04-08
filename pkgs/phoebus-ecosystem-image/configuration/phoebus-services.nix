bf327d35bdf0ce6106ef60bd5225f95f366ef859
Minijackson <minijackson@riseup.net>
2026-04-08 16:08:01 +0200

    phoebus-ecosystem-image: configure channel-finder

--- pkgs/phoebus-ecosystem-image/configuration/phoebus-services.nix
+++ pkgs/phoebus-ecosystem-image/configuration/phoebus-services.nix
@@ -27,6 +27,50 @@
       openFirewall = true;
     };
 
+    channel-finder = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        "server.http.port" = ports.channel-finder.port;
+        "demo_auth.enabled" = true;
+      };
+    };
+
+    recceiver = {
+      enable = true;
+
+      channelfinderapi.DEFAULT = {
+        BaseURL = "http://localhost:${toString ports.channel-finder.port}/ChannelFinder";
+        username = "admin";
+        password = "adminPass";
+      };
+
+      settings = {
+        recceiver = {
+          bind = "0.0.0.0:5050";
+
+          # When receiving metadata,
+          # print it on the command-line (show),
+          # and send it to ChannelFinder (cf).
+          procs = [
+            "show"
+            "cf"
+          ];
+        };
+        cf = {
+          # PV metadata to send to ChannelFinder
+          alias = "on";
+          recordDesc = "on";
+          recordType = "on";
+          environment_vars = {
+            EPICS_BASE = "EpicsBase";
+            EPICS_VERSION = "EpicsVersion";
+            PWD = "WorkingDirectory";
+          };
+        };
+      };
+    };
+
     # Phoebus dependencies
 
     elasticsearch = {
