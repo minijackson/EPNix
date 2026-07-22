556dece31342c80739e0ce26ab8a342b96f29128
Minijackson <minijackson@riseup.net>
2026-07-22 14:38:41 +0200

    phoebus-ecosystem: add description, license, and maintainership

--- pkgs/phoebus-ecosystem/qemu-vm.nix
+++ pkgs/phoebus-ecosystem/qemu-vm.nix
@@ -1,4 +1,4 @@
-{ epnixLib }:
+{ lib, epnixLib }:
 let
   finalSystem = epnixLib.inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
@@ -14,5 +14,10 @@ image
 // {
   passthru = image.passthru or { } // {
     inherit (finalSystem) config;
+  };
+  meta = image.meta or { } // {
+    description = "Phoebus ecosystem QEMU VM";
+    license = lib.licenses.asl20;
+    maintainers = with epnixLib.maintainers; [ minijackson ];
   };
 }
